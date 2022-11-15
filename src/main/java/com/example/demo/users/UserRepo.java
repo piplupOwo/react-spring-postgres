1e1901960250d14f7fd31b284334b4bdaee2f74b
piplupOwo <82926705+piplupOwo@users.noreply.github.com>
2022-11-15 16:06:23 +0800

    15/11

--- src/main/java/com/example/demo/users/UserRepo.java
+++ src/main/java/com/example/demo/users/UserRepo.java
@@ -1,8 +1,15 @@
 package com.example.demo.users;
 
-import com.example.demo.users.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<AppUser, Long> {
     AppUser findByUsername(String username);
+    AppUser findByConfirmationToken(String code);
+    
+    Optional<AppUser> findById(Long id);
+    Optional<AppUser> findByEmail(String email);
+    
+
 }
